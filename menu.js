5d0bf873967d8fbdada556875c91264c15aa8400
LeonardoTalanski <leonardo.e.t@hotmail.com>
2023-09-26 02:48:25 -0300

    colocando ./ em todas classes

--- menu.js
+++ menu.js
@@ -11,14 +11,14 @@ const logo = document.querySelector(".logo img");
 window.addEventListener("scroll", function() {
   if (window.scrollY > 820) {
     // Altera a logo para uma versão branca
-    logo.src = "assets/logo-branca-pt-2-min.jpg";
+    logo.src = "./assets/logo-branca-pt-2-min.jpg";
 
     // Define o tamanho da logo
     logo.width = 65;
     logo.height = 65;
   } else {
     // Altera a logo para a versão original
-    logo.src = "assets/logo-aws-cinza.png";
+    logo.src = "./assets/logo-aws-cinza.png";
 
     // Define o tamanho da logo
     logo.width = 65;
